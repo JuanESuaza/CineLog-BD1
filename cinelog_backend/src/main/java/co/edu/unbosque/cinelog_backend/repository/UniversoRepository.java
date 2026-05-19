d4584b82b4235e682dfc165ef288d7387003ee70
JuanESuaza <jsuaza@unbosque.edu.co>
2026-05-18 19:19:03 -0500

    Delete cinelog_backend directory

--- cinelog_backend/src/main/java/co/edu/unbosque/cinelog_backend/repository/UniversoRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.cinelog_backend.repository;
-
-import co.edu.unbosque.cinelog_backend.entity.Universo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UniversoRepository extends JpaRepository<Universo, Integer> {
-}
